7f7986946f143a64e8c0baa876e2c5f8e1acf5ec
My Duyen <dh51704984@student.stu.edu.vn>
2020-12-23 13:57:05 +0700

    hien thi san pham trang home

--- MNM/resources/views/User/Content/home.blade.php
+++ MNM/resources/views/User/Content/home.blade.php
@@ -66,7 +66,53 @@
             <div id="li-new-product" class="tab-pane active show" role="tabpanel">
                 <div class="row">
                     <div class="product-active owl-carousel">
-                        
+                        @foreach ($sanpham as $item)
+                        <div class="col-lg-12">
+                            <!-- single-product-wrap start -->
+                            <div class="single-product-wrap">
+                                <div class="product-image">
+                                    <a href="{{url("$item->ten_nhasanxuat/$item->ma_sp")}}">
+                                        <img src="./images/product/{{$item->urlhinh}}" alt="{{$item->tensp}}">
+                                    </a>
+                                    <span class="sticker">New</span>
+                                </div>
+                                <div class="product_desc">
+                                    <div class="product_desc_info">
+                                        <div class="product-review">
+                                            <h5 class="manufacturer">
+                                            <a href="./{{$item->ten_nhasanxuat}}">{{$item->ten_nhasanxuat}}</a>
+                                            </h5>
+                                            <div class="rating-box">
+                                                <ul class="rating">
+                                                    <li><i class="fa fa-star-o"></i></li>
+                                                    <li><i class="fa fa-star-o"></i></li>
+                                                    <li><i class="fa fa-star-o"></i></li>
+                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <h4><a class="product_name" href="{{url("$item->ten_nhasanxuat/$item->ma_sp")}}">{{$item->tensp}}</a></h4>
+                                        <div class="price-box">
+                                            <span class="new-price">
+                                                {{number_format($item->dongia)}} ₫
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="add-actions">
+                                        <ul class="add-actions-link">
+                                            <li class="add-cart active">
+                                                {{-- {{url('/addCart/'.$item->ma_sp)}} --}}
+                                                <a onclick="addCart({{$item->ma_sp}})" href="javascript:">Add to cart</a>
+                                            </li>
+                                            <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
+                                            <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -95,7 +141,52 @@
                     <div id="home1" class="tab-pane show fade in active">
                         <div class="row">
                             <div class="product-active owl-carousel">
-                                
+                                @foreach ($sanpham as $item)
+                                <div class="col-lg-12">
+                                    <!-- single-product-wrap start -->
+                                    <div class="single-product-wrap">
+                                        <div class="product-image">
+                                            <a href="{{url("$item->ten_nhasanxuat/$item->ma_sp")}}">
+                                                <img src="./images/product/{{$item->urlhinh}}" alt="{{$item->tensp}}">
+                                            </a>
+                                            <span class="sticker">New</span>
+                                        </div>
+                                        <div class="product_desc">
+                                            <div class="product_desc_info">
+                                                <div class="product-review">
+                                                    <h5 class="manufacturer">
+                                                    <a href="./{{$item->ten_nhasanxuat}}">{{$item->ten_nhasanxuat}}</a>
+                                                    </h5>
+                                                    <div class="rating-box">
+                                                        <ul class="rating">
+                                                            <li><i class="fa fa-star-o"></i></li>
+                                                            <li><i class="fa fa-star-o"></i></li>
+                                                            <li><i class="fa fa-star-o"></i></li>
+                                                            <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                            <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <h4><a class="product_name" href="{{url("$item->ten_nhasanxuat/$item->ma_sp")}}">{{$item->tensp}}</a></h4>
+                                                <div class="price-box">
+                                                    <span class="new-price">
+                                                            {{number_format($item->dongia)}} ₫
+                                                        </span>
+                                                </div>
+                                            </div>
+                                            <div class="add-actions">
+                                                <ul class="add-actions-link">
+                                                    <li class="add-cart active">
+                                                        <a onclick="addCart({{$item->ma_sp}})" href="javascript:">Add to cart</a>
+                                                    </li>
+                                                    <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
+                                                    <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                  @endforeach
                             </div>
                         </div>
                     </div>
